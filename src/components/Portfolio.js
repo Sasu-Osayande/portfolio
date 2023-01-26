0cb12db198799e1914f5fc93bedac536c6a4679d
Sasu Osayande <osayande.sasu@outlook.com>
2023-01-26 15:42:54 -0500

    Add icons for social media links

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,27 @@
 import "./Portfolio.css";
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
 
 function Portfolio() {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="Portfolio">
+      <form
+        action="https://github.com/Sasu-Osayande"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="Btn-icon">
+          <i class="fa-brands fa-github"></i>
+        </button>
+      </form>
+      <form
+        action="https://www.linkedin.com/in/sasuosayande/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="Btn-icon">
+        <i class="fa-brands fa-linkedin"></i>
+        </button>
+      </form>
     </div>
   );
 }
