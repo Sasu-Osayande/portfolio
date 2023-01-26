15b69eea446b769effa70d0e3eaa5549a60a205a
Sasu Osayande <osayande.sasu@outlook.com>
2023-01-26 14:34:19 -0500

    Add separate pages for entrance and portfolio

---
+++ src/components/Enter.js
@@ -0,0 +1,12 @@
+import "./Enter.css";
+// import {Link} from "react-router-dom";
+
+function Enter() {
+  return (
+    <div className="Enter">
+      <button className="Entrance">Click here to enter</button>
+    </div>
+  );
+}
+
+export default Enter;
---
+++ src/components/Portfolio.js
@@ -0,0 +1,12 @@
+import "./Portfolio.css";
+import {Link} from "react-router-dom";
+
+function Portfolio() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Portfolio;
