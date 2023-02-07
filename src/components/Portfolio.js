4fc02a51e5b8b33e3f1dd0a531efd16c34969605
Sasu Osayande <osayande.sasu@outlook.com>
2023-02-06 22:44:19 -0500

    Create button for each project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,33 +1,41 @@
 import "./Portfolio.css";
-// import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Portfolio() {
   return (
     <div className="Portfolio">
       <div className="Icons">
-      <form
-        action="https://github.com/Sasu-Osayande"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <button className="Btn-icon">
-          <i class="fa-brands fa-github"></i>
-        </button>
-      </form>
-      <form
-        action="https://www.linkedin.com/in/sasuosayande/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <button className="Btn-icon">
-        <i class="fa-brands fa-linkedin"></i>
-        </button>
-      </form>
+        <form
+          action="https://github.com/Sasu-Osayande"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="Btn-icon">
+            <i class="fa-brands fa-github"></i>
+          </button>
+        </form>
+        <form
+          action="https://www.linkedin.com/in/sasuosayande/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="Btn-icon">
+            <i class="fa-brands fa-linkedin"></i>
+          </button>
+        </form>
       </div>
       <div className="Projects">
-      <div className="Project">Sage</div>
-      <div className="Project">Fun-With-AI</div>
-      <div className="Project">Sprout</div>
+        <button className="Btn-project">
+          <Link to="/sage">
+            <div className="Project">Sage</div>
+          </Link>
+        </button>
+        <button className="Btn-project">
+          <div className="Project">Fun-With-AI</div>
+        </button>
+        <button className="Btn-project">
+          <div className="Project">Sprout</div>
+        </button>
       </div>
       <div>
         <div className="Blob"></div>
