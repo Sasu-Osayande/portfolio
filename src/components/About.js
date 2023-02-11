badc97ed1814f5ad9d0527f99103db7ad33292f7
Sasu Osayande <osayande.sasu@outlook.com>
2023-02-10 22:25:44 -0500

    Add video element

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,14 @@
 import "./About.css";
+import video from "../assets/videos/blob.mp4"
 // import { Link } from "react-router-dom";
 
 function About() {
   return (
     <div className="About">
       <span>About</span>
+      <video className="blob-video" autoplay playsinline>
+        <source src={video} type="video/mp4"/>
+      </video>
     </div>
   );
 }
